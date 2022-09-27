7ca35d45f059fda9c9200359f37ca46c6983ff6b
Charitha <Charitha.Neelisetty@ivycomptech.com>
2022-09-27 08:23:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Sept23_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3 };
+
+            for (int i = 0; i < arr.Length + 1; i++)
+            {
+                try
+                {
+                    Console.WriteLine(arr[i]);
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
